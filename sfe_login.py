bdee3e8b2464aa5ce8f7ebb2bf706b50691f91e5
Luke Wren <wren6991@gmail.com>
2016-12-20 22:25:05 +0000

    Updated SFE bot for new website changes

--- sfe_login.py
+++ sfe_login.py
@@ -1,12 +1,11 @@
 from selenium import webdriver
 from datetime import datetime
 import os
-import csv
 import smtplib
 
 from sfe_credentials import credentials
 
-csv_path = "sfe_correspondence.csv"
+csv_path = "sfe_correspondence.txt"
 sfe_date_format = "%d/%m/%Y"
 
 message_template = """\
@@ -22,24 +21,23 @@ This has been an automated message from SFE Bot :)
 Have a nice day!
 """
 
-def write_csv(filename, array2d):
+def write_correspondence(filename, data):
     ofile = open(filename, "w")
-    writer = csv.writer(ofile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-    for row in array2d:
-        writer.writerow(row)
+    ofile.write("\n".join(data))
     ofile.close()
 
-def read_csv(filename):
+def read_correspondence(filename):
     ifile = open(filename, "r")
-    reader = csv.reader(ifile, delimiter=',', quotechar='"')
-    return [row for row in reader if len(row) > 0]
+    rows = ifile.read().split("\n")
+    ifile.close()
+    return rows
 
 def send_email(new_correspondence = [], to = []):
-    with smtplib.SMTP_SSL(credentials.smtp_server) as server:
-        print(server.login(credentials.smtp_email, credentials.smtp_password))
-        correspondence_text = "\r\n".join([", ".join(r) for r in new_correspondence])
-        message_body = message_template % (credentials.smtp_email, ", ".join(to), correspondence_text)
-        server.sendmail(credentials.smtp_email, to, message_body)
+    with smtplib.SMTP_SSL(credentials["smtp_server"]) as server:
+        print(server.login(credentials["smtp_email"], credentials["smtp_password"]))
+        correspondence_text = "\r\n".join(new_correspondence)
+        message_body = message_template % (credentials["smtp_email"], ", ".join(to), correspondence_text)
+        server.sendmail(credentials["smtp_email"], to, message_body)
 
 # Set working directory to script directory:
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
@@ -67,21 +65,15 @@ for i in range(3):
 
 print("Navigating to correspondence...")
 browser.get("https://www.student-finance.service.gov.uk/customer/home/pages/correspondence/index")
-browser.switch_to.frame(browser.find_element_by_id("miniAppIframe"))
-# Chuck away the first and last rows of the table: these are the header and the navigation links
 print("Scraping data...")
-rows = browser.find_element_by_id("correspondenceTable").find_elements_by_tag_name("tr")[1:-1]
-data = [[td.text for td in tr.find_elements_by_tag_name("td")] for tr in rows]
+rows = browser.find_element_by_class_name("document-list").find_elements_by_tag_name("li")
+data = [r.text.replace("\n", "; ").replace("\r", "") for r in rows]
 
 if os.path.isfile(csv_path):
     print(csv_path + " already exists. Loading and comparing...")
-    old_data = read_csv(csv_path)
-    write_csv(csv_path, data)
-    last_known_date = datetime.strptime(old_data[0][0], sfe_date_format)
-    new_correspondence = []
-    for r in data:
-        if datetime.strptime(r[0], sfe_date_format) > last_known_date:
-            new_correspondence.append(r)
+    old_data = read_correspondence(csv_path)
+    write_correspondence(csv_path, data)
+    new_correspondence = [r for r in data if not r in old_data]
     print(new_correspondence)
     if len(new_correspondence) > 0:
         print("New correspondence discovered, sending email.")
@@ -90,6 +82,6 @@ if os.path.isfile(csv_path):
         print("No new correspondence discovered.")
 else:
     print(csv_path + " does not exist. Saving and exiting.")
-    write_csv(csv_path, data)
+    write_correspondence(csv_path, data)
 
 browser.close()
